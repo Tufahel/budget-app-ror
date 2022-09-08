a96f447df97aff28a4f82dec7487a28c28151544
Tufahel <tufahel96@gmail.com>
2022-09-09 01:53:20 +0600

    linters fixed

--- app/controllers/splash_screens_controller.rb
+++ app/controllers/splash_screens_controller.rb
@@ -1,15 +1,15 @@
 class SplashScreensController < ApplicationController
-    before_action :authenticate!
+  before_action :authenticate!
 
-    def index; end
-  
-    private
-  
-    def authenticate!
-      if user_signed_in?
-        redirect_to categories_path
-      else
-        redirect_to root_path
-      end
+  def index; end
+
+  private
+
+  def authenticate!
+    if user_signed_in?
+      redirect_to categories_path
+    else
+      redirect_to root_path
     end
+  end
 end
