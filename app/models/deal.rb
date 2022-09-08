49a8412dcfd22716eed3b17c41b444f24e633759
Tufahel <tufahel96@gmail.com>
2022-09-09 01:43:39 +0600

    fix bug deals-category

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,6 +1,6 @@
 class Deal < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :deals_categories, dependent: :destroy
+
   has_many :categories, through: :deals_categories
 
   validates :amount, presence: true
