0c98d903a6452eeb9bca9be7f53d16668e280b33
Tufahel <tufahel96@gmail.com>
2022-09-09 01:43:12 +0600

    update routes and migrate schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_07_130517) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_08_173030) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.float "amount"
     t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "icon"
     t.index ["author_id"], name: "index_categories_on_author_id"
   end
 
