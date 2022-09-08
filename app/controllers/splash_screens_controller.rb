1248717ccad4c87f3be4adb9b18cb40bea6ba001
Tufahel <tufahel96@gmail.com>
2022-09-09 01:43:55 +0600

    splash screen added

--- app/controllers/splash_screens_controller.rb
+++ app/controllers/splash_screens_controller.rb
@@ -1,2 +1,15 @@
 class SplashScreensController < ApplicationController
+    before_action :authenticate!
+
+    def index; end
+  
+    private
+  
+    def authenticate!
+      if user_signed_in?
+        redirect_to categories_path
+      else
+        redirect_to root_path
+      end
+    end
 end
